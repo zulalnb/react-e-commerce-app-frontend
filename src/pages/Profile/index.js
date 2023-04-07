9c7986a9ed614188a85f99f551e01417f0309120
Zülal Nebin <zllnbn1997@gmail.com>
2023-04-07 22:01:56 +0000

    Logout

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,13 +1,26 @@
-import { Text } from "@chakra-ui/react";
+import { Button, Text } from "@chakra-ui/react";
 import React from "react";
 import { useAuth } from "../../contexts/AuthContext";
 
-function Profile() {
-  const { user } = useAuth();
+function Profile({ history }) {
+  const { user, logout } = useAuth();
+
+  const handleLogout = async () => {
+    logout(() => {
+      history.push("/");
+    });
+  };
+
   return (
     <div>
       <Text fontSize="22">Profile</Text>
       <code>{JSON.stringify(user)}</code>
+
+      <br />
+      <br />
+      <Button colorScheme="pink" variant="solid" onClick={handleLogout}>
+        Logout
+      </Button>
     </div>
   );
 }
